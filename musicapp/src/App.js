4bb411f03bfa5e52c60ed7c9ca618ed88f1ee8b8
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2023-01-05 21:22:07 +0300

    add changes

--- musicapp/src/App.js
+++ musicapp/src/App.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, View, FlatList, StyleSheet} from 'react-native';
 import data from './data.json';
 import SongCard from './components/SongCard';
 import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
-  const renderSong = ({item}) => <SongCard song={item} />;
-  //bu şekilde her data için func oluşturmasın tekrardan diye.
+  const [list, setList] = useState(data);
+  const renderSong = ({item}) => <SongCard song={item} />; //her data için func oluşturmasın tekrardan diye.
 
   const renderSeperator = () => <View style={styles.seperator} />;
+  const handleSearch = text => {
+    const filteredList = data.filter(song => {
+      const searchedText = text.toLowerCase(); //input'a giirlen text
+      const currentTitle = song.title.toLowerCase(); //her item(song)
 
+      return currentTitle.indexOf(searchedText) > -1; //girilen text içinde index'leri eşleştir (0,1,2...)
+    });
+    setList(filteredList);
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <SearchBar />
+      <SearchBar onSearch={handleSearch} />
       <FlatList
         keyExtractor={item => item.id}
-        data={data}
+        data={list} //artık data değil değerimiz list(filtrelenen yani)
         renderItem={renderSong}
         ItemSeparatorComponent={renderSeperator} //üstte ve en alta seperatör koymaz
       />
